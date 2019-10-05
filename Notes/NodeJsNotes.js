25423d2cb324173bb67b7dcfe3133c997bb55753
mohamadAkthamObeidat <mohamadaktham7@gmail.com>
2019-10-05 10:49:20 +0300

    Initial Commint

---
+++ Notes/NodeJsNotes.js
@@ -0,0 +1,24 @@
+/* 
+    # 1 WHAT'S Node Js ?
+        - Node js Javascript Run Time Build on V8 Javascript Engine.
+        - It's An Environment In Which a Program Written In Javascript Can Be Executed Outside The Web Browser. 
+        - Node Js Runs && Parsed On V8 Engine That Developed By "Google".
+
+    # 2 WHY USE Node Js ?
+        - With Javascript We Can Build Very Fast JavaScript, Scalable Network Applications (Backend) 
+        - Single-Threaded Based On Event Driven, Non-Blocking I/O Model WHICH Makes Node Js Lightweight & Efficient.
+        - Node js Perfect For Building Super Fast & Scalable Data-Intensive Web Apps.
+
+    # 3 When To Use Node Js ?
+        - API With DataBase Behind It (Preferably NoSQL) Like (MongoDB & Firebase).
+        - Data Streaming (Think YouTube Or Netflix).
+        - Real-Time Chat Applications.
+        - Sever-Side Web Applications.
+        - The biggest Reason To Use Node Js Because It's Use The same Language Across Your Entire Stack Both On The front-End & Back-End.
+        - There's A Huge Libraries "NPM" Open-Source Packages Available For Every One For Free.
+        - There's A Very Huge Active Developers Community For Node Js.
+
+    # 4 When To Don't Use Node Js ?
+        - Applications With Heavy Server-Side Processing (CPU-Intensive) Like (Image Manipulating, Video Conversion & Files Compression).
+        - Alternative Languages For Node Js (Ruby on Rails, Python, PHP, etc...). 
+*/ 
